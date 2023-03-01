44d53f78228e23b12795c81f1603cf7b565558b4
krushnanikam60@gmail.com <krushnanikam60@gmail.com>
2023-03-01 15:19:52 +0530

    Spring first Project

---
+++ src/main/java/com/krushna/employeemanagementsystem/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.krushna.employeemanagementsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.krushna.employeemanagementsystem.domain.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+	// This will be AUTO IMPLEMENTED by Spring into a Bean
+}
